51e47da2db178d2a5e93111448d75faf4b49f5e6
Iheanyi Ekechukwu <iekechukwu@gmail.com>
2019-02-10 18:36:47 -0500

    Show off a ChildSpan.

--- services/pingersrv/server.go
+++ services/pingersrv/server.go
@@ -4,6 +4,7 @@ import (
 	"context"
 
 	"github.com/iheanyi/go-tracing-example/rpc/pinger"
+	opentracing "github.com/opentracing/opentracing-go"
 	"github.com/twitchtv/twirp"
 )
 
@@ -18,7 +19,13 @@ func (s *pingerServer) Ping(ctx context.Context, req *pinger.PingRequest) (*ping
 		return nil, twirp.InvalidArgumentError("message", "This can't be blank!")
 	}
 
+	childSpanDemo(ctx)
 	return &pinger.PingResponse{
 		Body: "Pong",
 	}, nil
+}
+
+func childSpanDemo(ctx context.Context) {
+	span, _ := opentracing.StartSpanFromContext(ctx, "ChildSpanCall")
+	defer span.Finish()
 }
